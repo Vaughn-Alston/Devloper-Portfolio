f9fdd76fd2d87730f88e35b0da1c89c55f25d7bb
Vaughn-Alston <vaughn2code@gmail.com>
2026-03-01 18:06:27 -0800

    Updated my boxes with new data

--- assets/js/app.js
+++ assets/js/app.js
@@ -46,28 +46,36 @@ let experience = [
 // The the description will describe the event.
 
 //Object one 
+
+
 {
- title: "AWS DeepRacer – Applied Reinforcement Learning Engineer",
- date: "11/15/24",
- description: "Designed and deployed an RL-based autonomous driving agent on AWS using PPO. Developed reward shaping strategies to improve policy learning efficiency and driving stability. Iteratively trained models in a cloud environment, optimizing for convergence and performance metrics. Supported live event infrastructure and debugging for autonomous vehicle execution.",
- image: "assets/images/aws.jpeg"
-
-},
-
-
+  title: "AI Skills Passport Offered by EY and Microsoft",
+  date: "2/16/26",
+  description: "Knowledge of core Artificial Intelligence and Machine Learning paradigms including supervised learning, unsupervised learning, and reinforcement learning. Understanding of model training workflows, reward shaping strategies, neural network fundamentals, and performance optimization techniques. Familiar with ethical AI principles, bias mitigation, and the application of AI systems for data pattern analysis and sustainability-focused decision support.",
+  image: "assets/images/cert.jpeg"
+ 
+ },
 
 
 
 
 // Object 2 and so on 
 {
- title: "San Deigo-Externship",
- date: "11/15/24",
- description: "......",
- image: "n/a"
+  title: "AWS DeepRacer – Applied Reinforcement Learning Engineer",
+  date: "11/15/24",
+  description: "Built and trained a reinforcement learning (RL) model on Amazon Web Services (AWS) to autonomously navigate a racing track. Designed a custom reward function to optimize lane centering and completion performance, iteratively refining policy behavior using Proximal Policy Optimization (PPO) in a cloud-based training environment. Supported live testing by assisting with hardware resets, monitoring model performance, and troubleshooting to ensure successful autonomous runs.",
+  image: "assets/images/aws.jpeg"
+ 
+ },
 
-},
-
+ {
+  title: "Externship UCSD Supercomputer Center",
+  date: "11/22/24",
+  description: "Participated in an HPC externship at the UCSD Supercomputer Center, gaining exposure to supercomputing architecture, energy consumption, and distributed systems. Completed a hands-on parallel processing simulation modeling how compute nodes collaborate to solve large-scale problems efficiently.",
+  image: "assets/images/supercom.jpg"
+ 
+ }
+ 
 
 
 
