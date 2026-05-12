a8285245f075bb392dd24d69979a660334a8488d
Vaughn-Alston <vaughn2code@gmail.com>
2026-05-11 22:38:47 -0700

    Updating my job description for DIAL - A - Ride

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,13 +48,31 @@ let experience = [
 //Object one 
 
 
+
+
 {
-  title: "City Of Manhattan Beach Dial-A-Ride Dispatcher",
+  title: "City of Manhattan Beach Dial-A-Ride Dispatcher",
   date: "2025 - Present",
-  description: "Coordinate senior transportation logistics and manage high-volume dispatch communications. Build trusted relationships with seniors and city personnel to ensure safe and reliable municipal service. Independently planned and executed a municipal program from concept through implementation, demonstrating leadership, accountability, and organizational coordination.",
+
+
+// Creating a nested array to hold array of things I done as bullet points
+  description: `
+  "Managed transportation scheduling and coordinated communication between drivers and senior residents.",
+
+• Developed a visual ride-tracking sticker system to improve accessibility for senior riders who struggled with handwriting or remembering ride schedules.
+
+• Designed the official Dial-A-Ride logo, later adopted for use in community newsletters and outreach materials.
+
+• Created and launched the “Dial-A-Ride Coffee Social” initiative to increase senior engagement and improve program ridership through organized transportation events.
+
+• Conducted outreach to local businesses, secured senior discounts, and coordinated transportation logistics for community participants.
+
+• Designed promotional flyers and collaborated with supervisors to successfully approve and launch public community programs.
+
+• Assisted with scheduling systems, customer service, operational coordination, and rider support in a fast-paced public service environment.
+`,
   image: "assets/images/healthfair.webp"
- 
- },
+},
 
 
 
